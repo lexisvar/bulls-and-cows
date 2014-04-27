273dcbdcfbf2aa6c22e56bb9f7df3b8cd382c7a6
Andrew <clickbuster1@gmail.com>
2014-04-28 02:37:13 +0300

    Added true MainController functionality, putting playerId into , to add redirect to home feature on every controller, need to tweak this a bit more to stop controller loading

--- assets/js/game/controllers/MainController.js
+++ assets/js/game/controllers/MainController.js
@@ -1,37 +1,22 @@
-angular.module('BullsAndCows').controller('SplashController', ['$scope', '$socket', '$location',
-  function ($scope, $socket, $location) {
-    'use strict';
+angular.module('BullsAndCows').controller('MainController', ['$scope', '$rootScope', '$socket', '$location',
+  function ($scope, $rootScope, $socket, $location) {
 
-    $scope.player = {
-      askName: false,
-      name: ''
-    };
-
-    $scope.showWelcome = function (res) {
-      // welcome logic
-      setTimeout(2000, function () {
-        $location.path('/lobby');
-      });
-    }
-
-    $scope.continueToLobby = function () {
-      $socket.get('/lobby/hasEntered', null, function (res) {
+    /**
+     * Test to see if we can identify the user, otherwise
+     * redirect to Splash Screen
+     */
+    if (undefined === $rootScope.playerId) {
+      // get name from API
+      $socket.get('/player/getName', null, function (res) {
         if (true === res.state) {
-          $scope.showWelcome(res);
-        } else {
-          $scope.$apply(function () {
-            $scope.player.askName = true;
+          $rootScope.$apply(function () {
+            $rootScope.playerId = res.name;
           });
+        } else {
+          $location.path('/');
         }
       });
     }
 
-    $scope.enterName = function () {
-      var data = {
-        name: $scope.player.name
-      };
-
-      $scope.get('/lobby/enter', data, $scope.showWelcome);
-    }
   }
-]);
+])
