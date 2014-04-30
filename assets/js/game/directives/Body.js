a5e0676df9e65aeea9f7c02102d2fc4b94fd5442
Andriyan Ivanov <andriyan.ivanov@gmail.com>
2014-04-30 16:13:14 +0300

    Moved resize body fix into a proper Angular directive

---
+++ assets/js/game/directives/Body.js
@@ -0,0 +1,24 @@
+angular.module('BullsAndCows').directive('body', ['$window', 
+  function($window) {
+    return {
+      restrict: 'E',
+      link: function bodyResize(scope, element, attributes) {
+        var heightConstraintsSum = 0, resize, property, value;
+        var properties = ['margin-top', 'margin-bottom', 'padding-top', 'padding-left'];
+
+        for(var x in properties) {
+          property = properties[x];
+          value = parseInt( element.css(property) );
+          heightConstraintsSum += isNaN(value) ? 0 : value;
+        }
+
+        (resize = function() {
+          var totalHeight = $window.innerHeight - heightConstraintsSum;
+          element.css('min-height', totalHeight + 'px' );
+        })();
+
+        angular.element($window).bind('resize', resize);
+      }
+    }
+  }
+]);
